d664f980e70d06401a2991cf10fb1bcc346cf210
Vivek Garg <gargwork321@gmail.com>
2024-08-28 14:58:18 +0530

    Initial Commit

---
+++ ApplesFramwork/ApplesFramworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplesFramworkApp.swift
+//  ApplesFramwork
+//
+//  Created by Vivek  Garg on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplesFramworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
