088e1d60558a044e4458b15cfc58d702d3976129
iOSApps100 <contact.vikram92@gmail.com>
2024-09-02 12:39:44 +0530

    left changes

--- ApplesFramwork/ApplesFramworkApp.swift
+++ ApplesFramwork/ApplesFramworkApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct ApplesFramworkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
+        
     }
 }
